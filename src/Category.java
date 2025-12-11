512deffa8a99f74d50be7993d5b06b66b7ad95ec
gavinc-3 <gavin_chen@horacemann.org>
2025-12-11 17:53:20 -0500

    added art

--- src/Category.java
+++ src/Category.java
@@ -1,12 +1,13 @@
 public class Category {
 
     String label;
-    String description; // after user is done and gets this category, will output description.
-    int points = 0;
+    String description;
+    String asciiArt;
 
-    Category(String c, String description) {
-        this.label = c;
+    Category(String label, String description, String asciiArt) {
+        this.label = label;
         this.description = description;
+        this.asciiArt = asciiArt;
     }
 
 }
